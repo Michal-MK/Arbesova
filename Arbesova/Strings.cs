9cf85e8523ee3a26a5bf79ed85d479c70b991591
Michal-MK <michalhz159@gmail.com>
2019-10-11 12:50:10 +0200

    Added some basic string operations and tests.

---
+++ Arbesova/Strings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Arbesova {
+	public class Strings {
+
+		//Check if the string is exactly 10 characters long
+		public bool Is10CharsLong(string str) {
+			return false;
+		}
+
+		//Check if the string contains even number of characters
+		public bool IsEvenLength(string str) {
+			return false;
+		}
+
+		//Check if the string contains odd number of characters
+		//Think before you implement this!
+		public bool IsOddLength(string str) {
+			return false;
+		}
+
+		//If the first letter of the string a capital letter ?
+		public bool StartsWithUppercase(string str) {
+			return false;
+		}
+
+		//Does the string contain exactly 'spaceCount' spaces ?
+		public bool ContainsXSpaces(int spaceCount, string str) {
+			return false;
+		}
+
+		//Does the string contain exactly 'spaceCount' of 'character' ?
+		public bool ContainsXCharacters(int charCount, string v, char character) {
+			return false;
+		}
+
+		//Is this string a valid sentence ?
+		public bool IsASentence(string str) {
+			return false;
+		}
+
+		//Return a string this time that contains a mirrored image of the input -> create a palindrome of even length
+		public string MirrorString(string toMirror) {
+			return "";
+		}
+
+		public string ReverseString(string toReverse) {
+			return "";
+		}
+	}
+}
---
+++ Tests/StringTests.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Arbesova;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests {
+	[TestClass]
+	public class StringTests {
+
+		private Strings strings = new Strings();
+
+		[TestMethod]
+		public void Is10CharsLong() {
+			Assert.IsTrue(strings.Is10CharsLong("0123456789"));
+			Assert.IsFalse(strings.Is10CharsLong(""));
+			Assert.IsFalse(strings.Is10CharsLong("0123456"));
+			Assert.IsFalse(strings.Is10CharsLong("asdxc"));
+			Assert.IsTrue(strings.Is10CharsLong("\t\t\t\t\t\t\t\t\t\t"));
+		}
+
+		[TestMethod]
+		public void IsEvenLength() {
+			Assert.IsTrue(strings.IsEvenLength(""));
+			Assert.IsFalse(strings.IsEvenLength("Hello"));
+			Assert.IsTrue(strings.IsEvenLength("Hello!"));
+			Assert.IsTrue(strings.IsEvenLength("d\t"));
+			Assert.IsTrue(strings.IsEvenLength("\t\t"));
+			Assert.IsFalse(strings.IsEvenLength("\t"));
+			Assert.IsFalse(strings.IsEvenLength("  "));
+		}
+
+		[TestMethod]
+		public void IsOddLength() {
+			Assert.IsFalse(strings.IsOddLength(""));
+			Assert.IsTrue(strings.IsOddLength("Hello"));
+			Assert.IsFalse(strings.IsOddLength("Hello!"));
+			Assert.IsFalse(strings.IsOddLength("d\t"));
+			Assert.IsFalse(strings.IsOddLength("\t\t"));
+			Assert.IsTrue(strings.IsOddLength("\t"));
+			Assert.IsTrue(strings.IsOddLength(" "));
+		}
+
+		[TestMethod]
+		public void StartsWithUppercase() {
+			Assert.IsTrue(strings.StartsWithUppercase("Hello"));
+			Assert.IsTrue(strings.StartsWithUppercase("H"));
+			Assert.IsFalse(strings.StartsWithUppercase("hello"));
+			Assert.IsFalse(strings.StartsWithUppercase(""));
+			Assert.IsFalse(strings.StartsWithUppercase("\t"));
+			Assert.IsTrue(strings.StartsWithUppercase("Get it?"));
+			Assert.IsTrue(strings.StartsWithUppercase("OMG"));
+			Assert.IsFalse(strings.StartsWithUppercase("123456"));
+			Assert.IsFalse(strings.StartsWithUppercase("."));
+		}
+
+		[TestMethod]
+		public void ContainsXSpaces() {
+			Assert.IsTrue(strings.ContainsXSpaces(0, "OMG"));
+			Assert.IsTrue(strings.ContainsXSpaces(1, "OMG_ "));
+			Assert.IsTrue(strings.ContainsXSpaces(2, "O M G"));
+			Assert.IsTrue(strings.ContainsXSpaces(0, "\t"));
+			Assert.IsTrue(strings.ContainsXSpaces(9, "Hello, my name is {insert name here}. How are you?"));
+			Assert.IsTrue(strings.ContainsXSpaces(0, ""));
+			Assert.IsTrue(strings.ContainsXSpaces(2, "  "));
+		}
+
+		[TestMethod]
+		public void ContainsXCharacters() {
+			Assert.IsTrue(strings.ContainsXCharacters(0, "OMG", 'p'));
+			Assert.IsTrue(strings.ContainsXCharacters(1, "OMG_ ", '_'));
+			Assert.IsTrue(strings.ContainsXCharacters(2, "O M G", ' '));
+			Assert.IsTrue(strings.ContainsXCharacters(0, "\t", ' '));
+			Assert.IsTrue(strings.ContainsXCharacters(3, "Hello, my name is {insert name here}. How are you?", 'm'));
+			Assert.IsTrue(strings.ContainsXCharacters(7, "0251681861.026849811.01897835480.0898413", '1'));
+			Assert.IsTrue(strings.ContainsXCharacters(0, "", 'a'));
+		}
+
+		[TestMethod]
+		public void IsASentence() {
+			Assert.IsTrue(strings.IsASentence("Hi."));
+			Assert.IsTrue(strings.IsASentence("Hello how are you?"));
+			Assert.IsTrue(strings.IsASentence("I am fine, Thanks!"));
+			Assert.IsTrue(strings.IsASentence("Good day sir."));
+			Assert.IsFalse(strings.IsASentence("Jeez"));
+			Assert.IsFalse(strings.IsASentence("this is GREAT!"));
+			Assert.IsFalse(strings.IsASentence(" "));
+			Assert.IsFalse(strings.IsASentence(" ."));
+			Assert.IsFalse(strings.IsASentence(".."));
+			Assert.IsFalse(strings.IsASentence("...Hello again!"));
+			Assert.IsFalse(strings.IsASentence("...Hello again,"));
+			Assert.IsFalse(strings.IsASentence("Hello again,"));
+			Assert.IsFalse(strings.IsASentence("and again!"));
+		}
+
+		[TestMethod]
+		public void MirrorString() {
+			Assert.IsTrue(strings.MirrorString("Hi.") == "Hi..iH");
+			Assert.IsTrue(strings.MirrorString("Hello how are you?") == "Hello how are you??uoy era woh olleH");
+			Assert.IsTrue(strings.MirrorString("Jeez") == "JeezzeeJ");
+			Assert.IsTrue(strings.MirrorString(" ") == "  ");
+			Assert.IsTrue(strings.MirrorString(" .") == " .. ");
+			Assert.IsTrue(strings.MirrorString("..") == "....");
+			Assert.IsTrue(strings.MirrorString("0123456789") == "01234567899876543210");
+			Assert.IsTrue(strings.MirrorString("") == "");
+			Assert.IsTrue(strings.MirrorString("Jelenovi pivo nelej") == "Jelenovi pivo nelejjelen ovip ivoneleJ");
+		}
+
+		[TestMethod]
+		public void ReverseString() {
+			Assert.IsTrue(strings.ReverseString("Hi.") == ".iH");
+			Assert.IsTrue(strings.ReverseString("Hello how are you?") == "?uoy era woh olleH");
+			Assert.IsTrue(strings.ReverseString("Jeez") == "zeeJ");
+			Assert.IsTrue(strings.ReverseString(" ") == " ");
+			Assert.IsTrue(strings.ReverseString(" .") == ". ");
+			Assert.IsTrue(strings.ReverseString("..") == "..");
+			Assert.IsTrue(strings.ReverseString("0123456789") == "9876543210");
+			Assert.IsTrue(strings.ReverseString("") == "");
+			Assert.IsTrue(strings.ReverseString("Jelenovi pivo nelej") == "jelen ovip ivoneleJ");
+		}
+	}
+}
